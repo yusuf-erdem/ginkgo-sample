8e4cdc491003274e1176cc44ec125c8891a276b3
Yusuf Erdem <81750669+yusuf-erdem@users.noreply.github.com>
2022-11-08 15:13:08 +0300

    Update sum.go

--- sum.go
+++ sum.go
@@ -7,6 +7,11 @@ func Sum(x, y int) int {
 	return x + y
 }
 
+func Sum2(x, y int) int {
+	fmt.Println("Received Digits :", x, y)
+	return x + y
+}
+
 func main() {
 	m, n := 3, 4
 	fmt.Println(Sum(m, n))
